474445c862913a16052ca43ef97b6d2159e1650a
wxg4957 <wxg4957@gmail.com>
2024-09-27 15:23:00 +0900

    0.4

--- week05/main.go
+++ week05/main.go
@@ -15,12 +15,14 @@ func main() {
 	//i = 55
 
 	i := 55
+	//i := "55"
 	f := 3.99
 
 	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 	fmt.Println(f, math.Ceil(3.49))
 	fmt.Println(strings.Title("kim inha"))
 	fmt.Printf("i는 %d\n", i)
+	//fmt.Printf("i는 %s\n", i)
 	fmt.Print("i는 ", i, "\n")
 	fmt.Println("i는", i)
 }
