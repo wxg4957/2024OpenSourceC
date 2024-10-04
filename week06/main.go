50ccf0d7a6d05ada7bd14186bb8497f52af92f0b
wxg4957 <wxg4957@gmail.com>
2024-10-04 15:39:11 +0900

    v1.0

--- week06/main.go
+++ week06/main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9
-	c1 := 'Z'
-	c2 := '김'
+	// i := 13
+	// var f float64 = 12.9
+	// c1 := 'Z'
+	// c2 := '김'
 
-	fmt.Printf("value: 1 : %d, value f : %f\n", i, f)
+	// fmt.Printf("value: 1 : %d, value f : %f\n", i, f)
 	// fmt.Printf("%d * %f = %f\n", i, f, i*f)
 	// fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // math.floor(f)
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	// fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // math.floor(f)
+	// fmt.Println(c1, c2)
+	// fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+
+	var f float64
+	var i int
+	var b bool
+	var s string
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i) // zero value
+	f = 2.7
+	i = 3
+	fmt.Print("\n", f < float64(i), "\n")
 }
