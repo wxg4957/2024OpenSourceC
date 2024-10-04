648542b32089415a565cc1dde1f1919a2ff3652e
wxg4957 <wxg4957@gmail.com>
2024-10-04 14:47:15 +0900

    v0.8

--- week06/main.go
+++ week06/main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	var i int = 13
-	f := 12.9
+	i := 13
+	var f float64 = 12.9
 	fmt.Printf("value: 1 : %d, value f : %f\n", i, f)
 	// fmt.Printf("%d * %f = %f\n", i, f, i*f)
-	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Println(reflect.TypeOf(i))
+	// fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
+	fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // math.floor(f)
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 }
