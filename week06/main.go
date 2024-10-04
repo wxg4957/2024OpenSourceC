24c5a299aa8fa45070b176395aa08335eccc525e
wxg4957 <wxg4957@gmail.com>
2024-10-04 14:27:03 +0900

    v0.7

---
+++ week06/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var i int = 13
+	f := 12.9
+	fmt.Printf("value: 1 : %d, value f : %f\n", i, f)
+	// fmt.Printf("%d * %f = %f\n", i, f, i*f)
+	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
+	fmt.Println(reflect.TypeOf(i))
+}
