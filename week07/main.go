5c46de31fe5d12806bc6b957d8420266a2024a4c
wxg4957 <wxg4957@gmail.com>
2024-10-11 14:01:16 +0900

    v1.3

--- week07/main.go
+++ week07/main.go
@@ -7,6 +7,12 @@ import (
 
 func main() {
 	var now time.Time = time.Now()
-	var month time.Month = now.Month()
-	fmt.Println(month)
+	var year int = now.Year()
+	var month int = int(now.Month())
+	var day int = now.Day()
+	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", year, month, day)
+
+	// fmt.Printf("오늘은 %d년 %d월 %d일 입니다.", now.Year(), int(now.Month)), now.Day())
+
+	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.", now.Hour(), now.Minute(), now.Second())
 }
