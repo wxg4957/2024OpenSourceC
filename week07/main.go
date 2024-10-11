d250b9d1c64e7b4b11024b6dc9eddb50b05ab8cf
wxg4957 <wxg4957@gmail.com>
2024-10-11 14:32:57 +0900

    v1.6

--- week07/main.go
+++ week07/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -15,14 +16,17 @@ func main() {
 	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", now.Year(), int(now.Month)), now.Day())
 	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.\n", now.Hour(), now.Minute(), now.Second()) */
 
-	// var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다."
-	// armyFixed := strings.NewReplacer("$", "국")
-	// fmt.Println(army)
-	// fmt.Println(armyFixed.Replace(army))
+	/* var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다."
+	armyFixed := strings.NewReplacer("$", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army)) */
 
 	in := bufio.NewReader(os.Stdin)
 	fmt.Print("input ur name: ")
 	name, err := in.ReadString('\n')
-	fmt.Println(name)
-	fmt.Println(err)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 }
