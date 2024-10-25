409debe25d64fd9cfbc48d3291412a87267187cc
wxg4957 <wxg4957@gmail.com>
2024-10-25 14:29:39 +0900

    v2.1

--- week09/main.go
+++ week09/main.go
@@ -17,24 +17,26 @@ func main() {
 	fmt.Println(answer)
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("숫자 입력: ")
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Print("숫자 입력: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(guess)
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guess)
 
-	if answer == guess {
-		fmt.Println("정답입니다.")
-	} else if answer > guess {
-		fmt.Println("입력하신 수는 정답보다 작은 수 입니다. Low")
-	} else {
-		fmt.Println("입력하신 수는 정답보다 큰 수 입니다. High")
+		if answer == guess {
+			fmt.Println("정답입니다.")
+		} else if answer > guess {
+			fmt.Println("입력하신 수는 정답보다 작은 수 입니다. Low")
+		} else {
+			fmt.Println("입력하신 수는 정답보다 큰 수 입니다. High")
+		}
 	}
 }
