432f98818cd2928459adedacd023fb02654c36fe
wxg4957 <wxg4957@gmail.com>
2024-10-25 14:24:35 +0900

    v2.0

--- week09/main.go
+++ week09/main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	target := rand.Intn(6)
-	fmt.Println(target)
+	answer := rand.Intn(6) + 1 //dice 1~6
+	fmt.Println(answer)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("숫자 입력: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(guess)
+
+	if answer == guess {
+		fmt.Println("정답입니다.")
+	} else if answer > guess {
+		fmt.Println("입력하신 수는 정답보다 작은 수 입니다. Low")
+	} else {
+		fmt.Println("입력하신 수는 정답보다 큰 수 입니다. High")
+	}
 }
