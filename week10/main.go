5b3a1795d2a6dd22f363744c8a5c72f3e81f343e
wxg4957 <wxg4957@gmail.com>
2024-11-01 15:36:51 +0900

    v3.3

--- week10/main.go
+++ week10/main.go
@@ -9,6 +9,31 @@ import (
 	"strings"
 )
 
+func isPrime(n int) bool {
+	// var isPrime bool = true
+	if n <= 1 {
+		// isPrime = false
+		return false
+	} else if n == 2 {
+		// isPrime = true
+		return true
+	} else if n%2 == 0 {
+		//isPrime = false
+		return false
+	} else {
+		j := 3
+		for j*j <= n {
+			if n%j == 0 {
+				// isPrime = false
+				// break
+				return false
+			}
+			j = j + 2
+		}
+	}
+	return true
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("정수 입력: ")
@@ -22,25 +47,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var isPrime bool = true
-	if n <= 1 {
-		isPrime = false
-	} else if n == 2 {
-		isPrime = true
-	} else if n%2 == 0 {
-		isPrime = false
-	} else {
-		j := 3
-		for j*j <= n {
-			if n%j == 0 {
-				isPrime = false
-				break
-			}
-			j = j + 2
-		}
-	}
-
-	if isPrime {
+	if isPrime(n) {
 		fmt.Printf("%d is prime number.", n)
 	} else {
 		fmt.Printf("%d is not prime number.", n)
