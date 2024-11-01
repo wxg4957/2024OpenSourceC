38ea0e2234b72d8619955101bad4419e52b9a4e8
wxg4957 <wxg4957@gmail.com>
2024-11-01 14:07:56 +0900

    v2.4

---
+++ week10/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("정수 입력: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = strings.TrimSpace(input)
+	n, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	counts := 0
+	j := 1
+	for j <= n {
+		if n%j == 0 {
+			counts++
+		}
+		j++
+	}
+	if counts == 2 {
+		fmt.Printf("%d is prime number.", n)
+	} else {
+		fmt.Printf("%d is not prime number.", n)
+	}
+}
