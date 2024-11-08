f7be004978f7c7a79f2d05156b389a1e3fbef8e7
wxg4957 <wxg4957@gmail.com>
2024-11-08 14:55:50 +0900

    v3.7

--- week11/main.go
+++ week11/main.go
@@ -3,6 +3,7 @@ package main
 import "week11/greeting"
 
 func main() {
-	greeting.Hello("인하")
-	greeting.Hi("inha")
+	// greeting.Hello("인하")
+	// greeting.Hi("inha")
+	greeting.EnglishGreetings("inha")
 }
