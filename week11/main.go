1373e9e3c46ee55f89793f37d52a08ec1a291ee6
wxg4957 <wxg4957@gmail.com>
2024-11-08 15:11:22 +0900

    v3.8

--- week11/main.go
+++ week11/main.go
@@ -1,9 +1,46 @@
 package main
 
-import "week11/greeting"
+import (
+	"fmt"
+	"log"
+	"week11/keyboard"
+)
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		j := 3
+		for j*j <= n {
+			if n%j == 0 {
+				return false
+			}
+			j = j + 2
+		}
+	}
+	return true
+}
 
 func main() {
-	// greeting.Hello("인하")
-	// greeting.Hi("inha")
-	greeting.EnglishGreetings("inha")
+	fmt.Print("시작값 입력: ")
+	n1, err := keyboard.GetInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("종료값 입력: ")
+	n2, err := keyboard.GetInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for j := n1; j <= n2; j++ {
+		if isPrime(j) {
+			fmt.Printf("%d ", j)
+		}
+	}
 }
