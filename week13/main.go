576b7c53976a0320cb9aac2d0e3ef38e73750d5e
wxg4957 <wxg4957@gmail.com>
2024-11-22 15:11:51 +0900

    v4.6

--- week13/main.go
+++ week13/main.go
@@ -2,26 +2,18 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
 )
 
 func main() {
-	var emptySlice []bool
-	// emptySlice = make([]bool, 5)
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice))
+	// fmt.Println(os.Args[1:], len(os.Args))
+	slices := os.Args[1:]
+	fmt.Println(slices[1])
 
-	if len(emptySlice) == 0 {
-		emptySlice = append(emptySlice, true)
+	for _, slice := range slices {
+		fmt.Println(slice)
 	}
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice))
 
-	var myArray [5]float64 = [5]float64{3.5, 4.1, 4.5, 2.9, 3.7}
-	mySlice := myArray[0:3]
-
-	// mySlice[1] = 2.2
-	myArray[1] = 2.2
-	mySlice = append(mySlice, 4.3, 1.1)
-
-	fmt.Println(len(myArray), myArray, reflect.TypeOf(myArray))
-	fmt.Println(len(mySlice), mySlice, reflect.TypeOf(mySlice))
+	slices = append(slices, "d", "e")
+	fmt.Println(slices, len(slices))
 }
